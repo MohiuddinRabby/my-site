4bb8a20e69b105abfe313df2018dd17933ccb358
MohiuddinRabby <rabbymohiuddin@gmail.com>
2020-06-29 17:07:44 +0600

    photo added-resize problem

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,21 +1,29 @@
 import React from 'react';
-import image from '../../assets/image/9.jpg';
+import image from '../../assets/image/4.jpg';
 import './Home.css';
 const Home = () => {
+  const primaryTextColor = {
+    // color1: '#3399ff',
+    color1: '#007399',
+    color2: '#004080',
+    borderRadious: '50%'
+  }
   return (
-    <div className="py-5">
+    <div className="py-4">
       <div className="container">
         <div className="row">
-          <div className="col-lg-6 col-md-6 col-sm-6 py-5">
+          <div className="col-lg-6 col-md-6 col-sm-6">
             <div className="about-description">
-              <h1 className="" style={{ color: '#0099e6' }}>Hi</h1>
+              <p className="display-4" style={{ color: primaryTextColor.color1 }}>Hello</p>
+              <h2 style={{ color: primaryTextColor.color1 }}>My Name is Mohiuddin</h2>
               <p>Welcome to my site. If you have landed on my site searching for me then I am glad you are interested about me, nice.</p>
-              <p>I am Mohiuddin (Golam Mohiuddin). I am a techy guy, who is passionate about softwares and programming. I’ve completed my bachelors degree in Software Engineering, and i'm from Bangladesh. My primary expertise is with <span className="react-text">React</span> I love to do things mostly with <span className="javascript-text">JavaScript</span>&nbsp;&nbsp;<span className="react-text" >React</span>&nbsp;&nbsp;<span className="bootstrap-text">Bootstrap</span></p>
-              <p>I use React and Linux on my day-to-day work. I am a fast learner and always searching to learn something new and interesting. I am very passionate about what I do.</p>
+              <p>I am a techy guy, who is passionate about softwares and programming. I’ve completed my bachelors degree in Software Engineering, and i'm from <span className="bangladesh-text">Bangladesh</span>. My primary interest is with <span className="react-text">React</span>. And I love to do things mostly with <span className="javascript-text">JavaScript</span>, <span className="react-text" >React</span>, <span className="bootstrap-text">Bootstrap</span></p>
+
+              <p>I use <span className="react-text" >React</span> and <span className="linux-text">Linux</span> on my day-to-day work. I am a fast learner and always searching to learn something new and interesting. I am very passionate about what I do.</p>
             </div>
           </div>
-          <div className=" col-lg-6  col-md-6">
-            {/* <img src={image} className="img-fluid" alt="" /> */}
+          <div className="col-lg-6  col-md-6 col-sm-6">
+            <img src={image} className="avatar img-fluid" style={{ borderRadius: '75%' }} alt="" />
           </div>
         </div>
       </div>
