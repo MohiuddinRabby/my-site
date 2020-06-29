377da82b2f797dbe2dd43d393ee326dec118757e
MohiuddinRabby <rabbymohiuddin@gmail.com>
2020-06-29 13:03:46 +0600

    home partly done

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,13 +5,14 @@ const Home = () => {
     <div className="py-5">
       <div className="container">
         <div className="row">
-          <div className="col-lg-6 col-md-6 col-sm-6">
-            <h3>Hello Ite Mohiuddin</h3>
-            <h2>I am a Web Developer</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores molestiae odit neque commodi ratione quo quos aspernatur. Provident rem excepturi saepe, asperiores perferendis hic vel eos iste, error modi omnis temporibus ad dolores qui eveniet. Cumque temporibus quas fuga aperiam dolores deserunt quis quisquam natus sed blanditiis doloribus, explicabo obcaecati?</p>
+          <div className="col-lg-6 col-md-6 col-sm-6 py-5">
+            <h1 className="" style={{ color: '#00802b' }}>Hi</h1>
+            <p>Welcome to my site. If you have landed on my site searching for me then I am glad you are interested about me, nice.</p>
+            <p>I am Mohiuddin (Golam Mohiuddin). I am a techy guy, who is passionate about softwares and programming. I’ve completed my bachelors degree in Software Engineering, and i'm from Bangladesh. My primary expertise is with React. I love to do things mostly with JavaScript and React.</p>
+            <p>I use React and Linux on my day-to-day work. I am a fast learner and I am always searching to learn something new and interesting. I am very passionate about what I do.</p>
           </div>
           <div className=" col-lg-6  col-md-6">
-              <img src={image} className="img-fluid" alt=""/>
+            {/* <img src={image} className="img-fluid" alt="" /> */}
           </div>
         </div>
       </div>
